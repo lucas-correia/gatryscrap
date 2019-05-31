b6846a116b30beb7b8aa25bcc5d22e106c52742c
Lucas Granados <>
2019-05-31 11:22:09 -0300

    loop added to get new promotions

--- gatry.sql
+++ gatry.sql
@@ -1,6 +1,6 @@
 create table promotion (
 id int primary key auto_increment,
-identifier varchar(15),
+identifier varchar(15) unique,
 name varchar(255) not null,
 url varchar(500),
 gatry_url varchar(500),
--- prog.py
+++ prog.py
@@ -2,9 +2,8 @@ import tutorial_conexao as cn
 import requests
 from bs4 import BeautifulSoup
 
-def savePromo(tuple):
+def savePromo(tuple, connection):
 	sql_query = "INSERT INTO promotion (`identifier`, `name`, `url`, `gatry_url`, `price`) VALUES (%s, %s, %s, %s, %s)"
-	connection = cn.connectDB()
 	try: 
 		cursor = connection.cursor(prepared=True)
 		result = cursor.execute(sql_query, tuple)
@@ -15,28 +14,38 @@ def savePromo(tuple):
 		print("erro ao inserir")
 	finally:
 		cursor.close()
-		cn.closeDB(connection)
-		print("conexao fechada")
+		
+connection = cn.connectDB()
+qtde=0
+while qtde<100:
+	url = 'https://gatry.com/home/mais_promocoes?onlyPromocao=true&qtde=' + str(qtde)
+	req = requests.get(url)
+	if req.status_code == 200:
+		print('gatry opened')
+		content = req.content
+		soup = BeautifulSoup(content, 'html.parser')
+		div = soup.find_all(name='div', attrs={'class':'informacoes'})
+		print(len(div))
 
-req = requests.get('http://www.gatry.com')
-conexao = cn.connectDB()
-if req.status_code == 200:
-	print('gatry opened')
-	content = req.content
-	soup = BeautifulSoup(content, 'html.parser')
-	div = soup.find_all(name='div', attrs={'class':'informacoes'})
-	print(len(div))
+		for item in div:
+			name_url = item.find(name='a')
+			price = item.find(attrs={'itemprop':'price'})
+			currency = item.find(attrs={'itemprop':'priceCurrency'})
+			try:
+				fullprice = currency.text + price.text
+			except:
+				fullprice = 'cupom'
+			id_url_gatry = item.find(name='a', attrs={'class':'mais hidden-xs'})
+			id_gatry = id_url_gatry['data-promocao']
+			url_gatry = id_url_gatry['href']
+			tupla = (id_gatry, name_url.text, name_url['href'], url_gatry, fullprice)
+			savePromo(tupla, connection)
+	qtde+=9
+cn.closeDB(connection)
+print("conexao fechada")
+		
 
-	for item in div:
-		name_url = item.find(name='a')
-		price = item.find(attrs={'itemprop':'price'})
-		currency = item.find(attrs={'itemprop':'priceCurrency'})
-		fullprice = currency.text + price.text
-		id_url_gatry = item.find(name='a', attrs={'class':'mais hidden-xs'})
-		id_gatry = id_url_gatry['data-promocao']
-		url_gatry = id_url_gatry['href']
-		tupla = (id_gatry, name_url.text, name_url['href'], url_gatry, fullprice)
-		savePromo(tupla)
+
 
 
 
